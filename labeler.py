f165103b2ee9ed68ed3a254e15edf1d2ea1a6d29
Cybertechy <siddhtailor96@gmail.com>
2025-02-16 23:52:17 +0400

    file renaming and column renaming

--- labeler.py
+++ labeler.py
@@ -138,6 +138,7 @@ def sidebar_controls():
     if jump - 1 != st.session_state.index:
         st.session_state.index = jump - 1
 
+
     # Export all the data combined
     if st.sidebar.button("Export All Data"):
         # select all the files in the Data folder
@@ -151,6 +152,28 @@ def sidebar_controls():
         # download the combined file
         csv_data = df.to_csv(index=False).encode("utf-8")
         st.sidebar.download_button(label="Download Combined CSV", data=csv_data, file_name="combined_data.csv", mime="text/csv")
+
+    # Export all the data combined
+    if st.sidebar.button("Export All Data"):
+        # select all the files in the Data folder
+        csv_files = glob.glob(os.path.join("Data", "*.csv"))
+        # read all the files and concatenate them
+        df = pd.concat([pd.read_csv(file, engine='pyarrow') for file in csv_files])
+        
+        # Rename columns in the DataFrame
+        df = df.rename(columns={
+            'label_1': 'roberta_label',
+            'score_1': 'roberta_score',
+            'm_label_1': 'manual_label'
+        })
+        
+        try:
+            # Create CSV data for download
+            csv_data = df.to_csv(index=False).encode("utf-8")
+            st.sidebar.download_button(label="Download Combined CSV", data=csv_data, file_name="round_manual_low_confidence.csv", mime="text/csv")
+            st.sidebar.success("Data ready for download!")
+        except Exception as e:
+            st.sidebar.error(f"Error preparing download: {str(e)}")
         
 
 def display_full_labeling():
